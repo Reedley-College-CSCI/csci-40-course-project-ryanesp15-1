8233718c474fd6e767c3e1f6ba349175b0c150fc
ryanesp15 <0489698@MY.SCCCD.EDU>
2025-11-10 20:04:20 -0800

    *deleted other .cpp, still unable to run, will try after i commit.

---
+++ theMain.cpp
@@ -0,0 +1,15 @@
+// CSCI-40 Final Project
+// Author: [Ryan Esparza]
+
+/*
+ * Description:This code allows simple data collection on pitchers in the sport baseball.
+ *
+*/
+
+#include <iostream>
+
+int main() {
+
+    cout << "test";
+    return 0;
+}
